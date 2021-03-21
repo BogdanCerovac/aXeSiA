d8cd786828818278fb019601bbafb64f822e78f3
BogdanCerovac <bogdan.cerovac@gmail.com>
2021-03-21 19:31:46 +0100

    Added siteimprove summary to show all DB test

--- util/dbShowAll.js
+++ util/dbShowAll.js
@@ -6,7 +6,9 @@ const {stats} = require('./DB');
 // JSON example: https://stackoverflow.com/questions/33432421/sqlite-json1-example-for-json-extract-set/33433552
 
 const selectAll = db.prepare(
-    `SELECT id, ts, url, json_extract(audit, '$.aXeAudit') as aXeAudit, json_extract(audit, '$.lighthouseAudit') as lighthouseAudit FROM audits`,
+    `SELECT 
+        id, ts, url, json_extract(audit, '$.aXeAudit') as aXeAudit, json_extract(audit, '$.lighthouseAudit') as lighthouseAudit , json_extract(audit, '$.siteimproveAudit') as siteimproveAudit 
+    FROM audits`,
 );
 let selectedAll = selectAll.all();
 
