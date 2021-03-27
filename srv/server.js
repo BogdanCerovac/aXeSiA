209b01d256d3b4573730cb53af38dd41c50d2984
BogdanCerovac <bogdan.cerovac@gmail.com>
2021-03-27 01:31:14 +0100

    Added simple templating from express with handlebars, so that we can pass data to frontend

--- srv/server.js
+++ srv/server.js
@@ -1,20 +1,54 @@
 var express = require('express');
+const handlebars = require('express-handlebars');
 var app = express();
 var path = require('path');
 
-
 const PORT = process.env.PORT || 80;
 
-
 // Simple log
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     console.log(new Date().toISOString() + ' Request URL:', req.originalUrl);
     next()
-})
+})*/
 
 // Serve Static Assets
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/res', express.static(path.join(__dirname, 'public/res')));
 
+// Handlebars
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'/handlebars'));
+
+
+
+app.get('/', function (req, res, next) {
+
+    console.log(new Date().toISOString() + ' Request URL:', req.originalUrl);
+
+    res.render('home', {
+        titleEnd: "home",
+        helpers: {
+            foo: function () { return 'foo.'; },
+            bar: function () { return 'bar.'; }
+        },
+        data: function(){
+            return JSON.stringify([
+                {
+                    name: "Test 1",
+                    value: 1
+                },
+                {
+                    name: "Test 2",
+                    value: 2
+                },
+                {
+                    name: "Test 3",
+                    value: 3
+                }
+            ])
+        }
+    });
+});
 
 // start server
 app.listen(PORT, () => {
