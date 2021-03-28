c9b41f85f79eea76de0d4692646ed6502409f5df
BogdanCerovac <bogdan.cerovac@gmail.com>
2021-03-28 13:38:59 +0200

    Axe details summary subdetails as a start

--- srv/public/res/script.js
+++ srv/public/res/script.js
@@ -20,10 +20,51 @@ document.querySelectorAll('a[target="_blank"]').forEach(link => {
     addNewTabMessage(link);
 });
 
+//url as id has to be cleaned
+function cleanUrl(url){
+    return url.replace(/[^a-zA-Z]/g, '_');
+}
+
 
 //render details based on type
 function renderDetails(details, type){
+ 
     // TODO
+    switch (type) {
+        case 'axe':
+           
+            let violationsHTML = `<p>Violations: ${details.violations}</p>`;
+            if(details.violationsImpacts.length > 0){
+                violationsHTML += `<p>Violation impacts:</p><ul>`;
+                let violationImpacts = '';
+                details.violationsImpacts.map(impact => violationImpacts += `<li>${impact}</li>`);
+                violationsHTML += `${violationImpacts}</ul>`;
+            }
+
+            if(details.violationsTags.length > 0){
+                violationsHTML += `<p>Violation tags:</p><ul>`;
+                let violationTags = '';
+                details.violationsTags.map(tag => violationTags += `<li>${tag}</li>`);
+                violationsHTML += `${violationTags}</ul>`;
+            }
+
+            return `
+            <div class="details axe-details">
+                <p>Time: ${details.time} ms</p>
+                <p>Passes: ${details.passes}</p>
+                ${violationsHTML}
+                <p>Incomplete: ${details.incomplete}</p>
+                <p>Inapplicable: ${details.inapplicable}</p>
+                <p>Total: ${details.total}</p>
+            </div>
+          `;
+        case 'lh':
+        case 'si':
+          
+          break;
+        default:
+          console.log(`Sorry, ${type} not supported yet.`);
+      }
 }
 
 
@@ -32,24 +73,25 @@ function generateDetails(type, data){
     /*console.log(data)
     */
     let out = '';
-    for(const key in data){
-        const meta = data[key];
+    for(const url in data){
+        const meta = data[url];
 
         let details = '';
         const metaSorted = meta.sort((a, b) => (new Date(a.ts).getTime() < new Date(b.ts).getTime()) ? 1 : -1)
         metaSorted.map( detail => {
-
+            const subDetails = renderDetails(detail[type], type);
             details += `<details class="details-sub-trigger">
             <summary>${detail.ts}</summary>
                 <div class="details-sub">
-                ${JSON.stringify(detail[type])}
+                <pre style="white-space: break-spaces;">${JSON.stringify(detail[type])}</pre>
+                ${subDetails}
                 </div>
             </details>`;
         })
          
         out += `
-        <details class="details-main-trigger">
-            <summary>${key}</summary>
+        <details class="details-main-trigger" id="${cleanUrl(url)}">
+            <summary>${url}</summary>
             <div class="details-main">${details}</div>
         </details>
         `;
