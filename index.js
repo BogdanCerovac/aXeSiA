8ceb0bac6d127d2198d782dc433fe0539a79b5ea
BogdanCerovac <bogdan.cerovac@gmail.com>
2021-04-15 23:43:27 +0200

    Index updated

--- index.js
+++ index.js
@@ -18,7 +18,8 @@
 const Sitemapper = require('sitemapper');
 const puppeteer = require('puppeteer');
 let db = require('./util/DB');
-
+const {todaysDate} = require('./util/helpers');
+ 
 const {
   iPhone4,
   iPhone4_land,
@@ -45,17 +46,20 @@ const {getScreenShotsForAllDevices} = require('./tasks/urlTaskGetScreenshots');
 const {getAXEreportForURL} = require('./tasks/urlTaskGetAxeAudit');
 const {getLighthouseReportForURL} = require('./tasks/urlTaskGetLighthouse');
 const {getSiteimproveAlphaReportForURL} = require('./tasks/urlTaskGetSiteimproveAudit');
+const urlResponseChecker = require('./tasks/urlResponseChecker');
+const urlTaskGetSiteComplexity = require('./tasks/urlTaskGetSiteComplexity');
+const urlTaskGetSiteEvents = require('./tasks/urlTaskGetSiteEvents');
 
 /******************* CONFIG *********************/
 
-let mainCFG = {
+/*let mainCFG = {
   mainURL: 'https://cerovac.com/a11y/sitemap.xml',
   cookieConsent: false,
   pathForScreenshots : './out/screens/test/'
-};
+};*/
 
-/*
-mainCFG = {
+
+let mainCFG = {
   mainURL: 'https://www.itumx.no/sitemap.xml',
   cookieConsent: {
     selector: '#onetrust-accept-btn-handler',
@@ -63,7 +67,7 @@ mainCFG = {
   },
   pathForScreenshots : './out/screens/test/'
 };
-*/
+
 
 /***********************************************/
 
@@ -130,7 +134,7 @@ const started = new Date();
         console.error("No sites to process. Please check if sitemap.xml is working etc.")
         process.exit(1);
       }
-      console.log(sitesNum + " unique sites to be processed.");
+      console.log(sitesNum + " unique URLs to be processed.");
 
       // Puppeteer browser object
       const browserObj = await puppeteer.launch({
@@ -146,27 +150,100 @@ const started = new Date();
       //db stats before loop
       console.log(db.stats());
 
+      //set unique id for whole audit
+      const id_audit = todaysDate() + "--" + new Date().getTime();
+
       // main loop
-      let counter = 0;
+      let counterAll = 0;
+
+      let urls_processed_ok = [];
+      let urls_processed_error = [];
+      let urls_not_reachable = [];
+
+      let auditsDoneSuccessfully = 0;
+
       for (const i in sitesUnique) {
         let url = sitesUnique[i];
         //console.log(url)
-        console.log("Running tasks for url " + (counter + 1) + " of " + sitesNum);
-        //const screens = await getScreenShotsForAllDevices(browserObj, devicesForScreenshots, url, mainCFG.pathForScreenshots);
-        const aXeAudit = await getAXEreportForURL(browserObj, url);
-        const lighthouseAudit = await getLighthouseReportForURL(browserObj, url);
-        const siteimproveAudit = await getSiteimproveAlphaReportForURL(browserObj, url);
-        const dbRes = db.insert(domain, url, JSON.stringify({aXeAudit , lighthouseAudit, siteimproveAudit}));
-        //console.log(dbRes);
-        counter++;
+        console.log("Running tasks for URL nr. " + (counterAll + 1) + " of " + sitesNum);
+        
+        // check if url works at all
+        const urlResponseCheck = await urlResponseChecker(url);
+        if(urlResponseCheck === true){
+
+          // some tasks may cause exceptions, let's handle them
+          try {
+
+            //const screens = await getScreenShotsForAllDevices(browserObj, devicesForScreenshots, url, mainCFG.pathForScreenshots);
+            const aXeAudit = await getAXEreportForURL(browserObj, url);
+            const lighthouseAudit = await getLighthouseReportForURL(browserObj, url);
+            const siteimproveAudit = await getSiteimproveAlphaReportForURL(browserObj, url);
+            
+            const siteComplexity = await urlTaskGetSiteComplexity(browserObj, url);
+            const siteEvents = await urlTaskGetSiteEvents(browserObj, url);
+            
+            let siteComplexityStats = {...siteComplexity, ...siteEvents };
+            siteComplexityStats.timeSum = siteComplexity.timeEls + siteEvents.timeEvents;
+
+            const dbRes = db.insert(id_audit, domain, url, JSON.stringify({aXeAudit , lighthouseAudit, siteimproveAudit, siteComplexityStats}));
+
+            urls_processed_ok.push(url);
+            
+          } catch (error) {
+            console.error(url + " audit not completed OK! Error: ", error)
+            urls_processed_error.push(url);
+          }
+ 
+
+        }else{
+          console.warn(url + " response was not OK, we got ", urlResponseCheck);
+          urls_not_reachable.push(url);
+        }
+
+        counterAll++;
       }
 
+      let num_urls_processed_ok = urls_processed_ok.length;
+      let num_urls_processed_error = urls_processed_error.length;
+      let num_urls_not_reachable = urls_not_reachable.length;
+
       // check overall status
-      if(sitesNum === counter){
-        console.log("All " + counter + " urls processed OK");
+      if(sitesNum === counterAll && sitesNum === num_urls_processed_ok){
+
+        console.log("All " + counterAll + " URLs working OK and processed OK");
+        auditsDoneSuccessfully = 1;
+
       }else{
-        console.warn("Only " + counter + " of " + sitesNum + " urls processed");
+
+        if(num_urls_processed_error === 0){
+
+          auditsDoneSuccessfully = 1;
+
+          console.log(num_urls_processed_ok + " of " + sitesNum + " URLs was processed OK. ");
+
+
+        }else{
+
+          console.error(num_urls_processed_error + " of " + sitesNum + " URLs FAILED, these audits can therefore not represent correct status and will be excluded!");
+
+        }
       }
+
+      if(num_urls_not_reachable > 0){
+
+        console.warn(num_urls_not_reachable + " URLs was not reachable and should be investigated manually and ideally removed from sitemap.xml if they are not relevant");
+      }
+
+      // add to audit meta
+      // (id_audit, domain, urls_all_num, urls_all, urls_ok_num, urls_ok, urls_err_num, urls_err, urls_na_num, urls_na, audit_ok)
+      const metaAuditInsert = db.insertMeta(
+        id_audit, domain, 
+        sitesUnique.length, JSON.stringify(sitesUnique), 
+        num_urls_processed_ok, JSON.stringify(urls_processed_ok), 
+        num_urls_processed_error, JSON.stringify(urls_processed_error),
+        num_urls_not_reachable, JSON.stringify(urls_not_reachable),
+        auditsDoneSuccessfully
+      );
       
       // db stats after
       console.log(db.stats());
